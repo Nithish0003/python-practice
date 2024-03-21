6fea28afd184daa0fe7f451ec15d10e9910ae184
Nithish0003 <nits000333@gmail.com>
2024-03-21 15:27:19 +0530

    commit-2

---
+++ basics/case.py
@@ -0,0 +1,8 @@
+#Normal
+firstvariableformyenvironment = 10
+#camelCase
+firstVariableForMyEnvironment = 10
+#PascalCase
+FirstVariableForMyEnvironment = 10
+#Snake case
+first_variable_for_my_environment = 10
