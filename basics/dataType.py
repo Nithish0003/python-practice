aff738d31ca63d2a3683570d862eb228c7c4520f
Nithish0003 <nits000333@gmail.com>
2024-03-21 16:59:44 +0530

    data type added

---
+++ basics/dataType.py
@@ -0,0 +1,32 @@
+# set = {"Luffy","Zoro","YUsap"}
+# print(type(set))
+
+# dict={"Name":"Luffy","age":19,"HomeTown":"LougeTale"}
+# print(type(dict))
+
+# print(dict["Name"].upper())
+
+str="Viper"
+print(str.upper())
+str1="SOVA"
+print(str1.lower())
+str2="phenoix"
+print(str2.capitalize())
+str3="green chilly"
+print(str3.find("l"))
+str4="red chilly"
+print(str4.rfind("l"))
+str5="pachaiappan"
+print(str5.count("p"))
+str6="sarpetta parambarai"
+print("index of",str6,"is:",str.index("p"))
+str7="abc aaab ccca bcd"
+print(str7.strip("b"))
+str8="virtual box"
+print(str8.rstrip("box"))
+str9="visual studio"
+print(str9.lstrip("visual"))
+str10="welcome,to,great,girikalan magic show"
+print(str10.split(","))
+str11="madaki kutthu"
+print(str11.replace("ma","kp"))
