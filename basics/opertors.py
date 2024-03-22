a78640802ce111fe242b90502a068024c54bdeaf
Nithish0003 <nits000333@gmail.com>
2024-03-22 18:10:21 +0530

    updated

---
+++ basics/opertors.py
@@ -0,0 +1,30 @@
+a=8 
+b=6
+# Arithmetic
+print(a+b)
+print(a-b)
+print(a*b)
+print(b%a)
+print(a/b)
+print(a//b)
+print("-------------------------")
+# comparison
+print(a==b)
+print(a>b)
+print(a<b)
+print(a!=b)
+# logical
+print("-------------------------")
+if(a==8 and b==5):
+    print(True)
+else:
+    print(False)
+if(a==8 or b==5):
+    print(True)
+else:
+    print(False)
+c=True
+if(not c):
+    print(True)
+else:
+    print(False)
---
+++ task/cal.py
@@ -0,0 +1,15 @@
+a=5
+b=10
+opera="mul"
+if opera=="add":
+    print(a+b)
+elif opera=="sub":
+    print(a-b)
+elif opera=="mul":
+    print(a*b)
+elif opera=="div":
+    print(a/b)
+elif opera=="mod":
+    print(a%b)
+elif opera=="floor":
+    print(a//b)
